f9959b013e99b5a2b9798e50ef4dceaec346d97a
Wahmane-Hamza <hamza.wahm5@gmail.com>
2026-04-13 19:29:32 +0100

    cpp06

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -8,7 +8,9 @@ int	main()
 	uintptr_t	raw = Serialize::serialize(ptr1);
 	Data*		ptr2 = Serialize::deserialize(raw);
 
+	std::cout << "raw: " << raw << std::endl;
 	std::cout << "ptr1: " << ptr1 << std::endl;
+	std::cout << "raw: " << raw << std::endl;
 	std::cout << "ptr2: " << ptr2 << std::endl;
 
 	std::cout << "Is Equals: " << (ptr1 == ptr2 ? "True" : "false") << std::endl;
